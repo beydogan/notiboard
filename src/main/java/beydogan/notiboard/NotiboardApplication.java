5644bd24786331b75967b7eeed41121186230e3d
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2017-01-15 23:00:17 +0300

    Initial commit with spring initializr

---
+++ src/main/java/beydogan/notiboard/NotiboardApplication.java
@@ -0,0 +1,12 @@
+package beydogan.notiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiboardApplication.class, args);
+	}
+}
