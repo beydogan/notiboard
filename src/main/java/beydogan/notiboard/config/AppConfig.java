cc12804048bed7e41379e09304e9202981cfc07b
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2017-01-17 17:48:06 +0300

    Add Layout and Config for layout(WIP)

--- src/main/java/beydogan/notiboard/config/AppConfig.java
+++ src/main/java/beydogan/notiboard/config/AppConfig.java
@@ -1,13 +1,16 @@
 package beydogan.notiboard.config;
 
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 import javax.sql.DataSource;
 
@@ -25,12 +28,21 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return dataSource;
     }
 
-    @Bean
-    public ServletContextTemplateResolver templateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        resolver.setCacheable(false);
-        return resolver;
-    }
+//    @Bean
+//    public ServletContextTemplateResolver templateResolver() {
+//        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+//        resolver.setSuffix(".html");
+//        resolver.setTemplateMode("HTML5");
+//        resolver.setCacheable(false);
+//        return resolver;
+//    }
+//
+//
+//    @Bean
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.addTemplateResolver(templateResolver());
+//        templateEngine.addDialect(new LayoutDialect());
+//        return templateEngine;
+//    }
 }
